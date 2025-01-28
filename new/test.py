00ccdbf730877cc210d9dd62350a597024f0ce74
Shinsukehun <itoshinsuke16@gmail.com>
2025-01-28 13:07:25 +0100

    new test

---
+++ new/test.py
@@ -0,0 +1,23 @@
+# fruits=["apple","banana","cherry","tomato"]
+# for fruit in fruits:
+#     print(fruit)
+# count=0
+# while count<3:
+#     print(fruits[count])
+#     count=count+1
+def addnumbers(a,b):
+    return a+b
+c=addnumbers(2,3)
+print(c)
+i=0
+for i in range(100):
+    print(i)
+    i=i+1
+def multiply(a,b):
+    return a*b
+a=int(input())
+b=int(input())
+c=multiply(a,b)
+print(c)
+fruit=["apple","banana","cherry","tomato"]
+print(fruit[1:4])
